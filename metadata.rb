e3013715d690531b788f95bd1a2051efbb416b41
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2021-03-02 09:58:19 +0400

    A generated key's name corresponds now to the Chef resource name property

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
 description 'Create DH parameters files'
-version '2.0.1'
+version '2.0.2'
 
 scm_url = 'https://github.com/aspyatkin/dhparam-cookbook'
 source_url scm_url
--- resources/file.rb
+++ resources/file.rb
@@ -16,11 +16,11 @@ action :create do
   ::ChefCookbook::DHParam.add(
     node,
     new_resource.name,
-    ::File.join(new_resource.path, "#{new_resource.key_length}.pem")
+    ::File.join(new_resource.path, "#{new_resource.name}.pem")
   )
   file = ::ChefCookbook::DHParam.file(node, new_resource.name)
 
-  execute "generate #{new_resource.key_length}-bit DH parameters file at #{new_resource.path}" do
+  execute "generate #{new_resource.key_length}-bit DH parameters file at #{file}" do
     command(lazy { "openssl dhparam -out #{file} #{new_resource.key_length}" })
     action :run
     not_if { ::File.exist?(file) }
